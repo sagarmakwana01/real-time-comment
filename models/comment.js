62914efed9269a22d8472bf4abc356b918f93635
Sagar Makwana <116143298+sagarmakwana01@users.noreply.github.com>
2023-04-29 11:26:40 +0530

    Add files via upload

---
+++ models/comment.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema({
+    username:{type: String, required: true},
+    comment:{type:String, required: true}
+},{timestamps: true})
+
+module.exports = mongoose.model('Comment', commentSchema);
